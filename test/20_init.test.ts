a002a9d9b26ec7670a71bbe717042787569b5b4f
waiting <1661926154@qq.com>
2018-01-21 20:33:10 +0800

    test: Should initDefaultCenter() works

--- test/20_init.test.ts
+++ test/20_init.test.ts
@@ -4,8 +4,10 @@
 import { basename, normalize } from 'path'
 import * as assert from 'power-assert'
 
+import * as rmdir from 'rimraf'
+
 import * as myca from '../src/index'
-import { isDirExists } from '../src/lib/common'
+import { isDirExists, unlinkAsync } from '../src/lib/common'
 import { config } from '../src/lib/config'
 
 const filename = basename(__filename)
@@ -15,16 +17,45 @@ config.userHome = config.isWin32 ? normalize(process.env.USERPROFILE || '') : no
 config.defaultCenterPath = normalize(`${config.userHome}/${config.centerDirName}`) // dir contains conf file and folders
 config.openssl = normalize(config.openssl)
 
-let isDefaultCenterInited = false
 
 describe(filename, () => {
+  beforeEach(() => {
+    config.defaultCenterPath = normalize(`${config.userHome}/${config.centerDirName}`)
+  })
+
+  it('Should initDefaultCenter() works', async () => {
+    const random = Math.random()
+    const randomPath = `${config.userHome}/myca-test-${random}`
+
+    config.defaultCenterPath = `${randomPath}/${config.centerDirName}`
+
+    try {
+      await myca.initDefaultCenter()
+    }
+    catch (ex) {
+      return assert(false, ex)
+    }
+
+    if ( ! await isDirExists(config.defaultCenterPath)) {
+      return assert(false, `default center folder not exists, path: "${config.defaultCenterPath}"`)
+    }
+
+    assert(
+      await myca.isCenterInited('default'),
+      `isCenterInited('default') says folder not exits. path: "${config.defaultCenterPath}"`)
+
+    rmdir(randomPath, (err) => {
+      err && console.error(err)
+    })
+  })
+
 
   it('Should getCenterPath() works', async () => {
     const centerPath = await myca.getCenterPath('default')
     const isDefaultCenterInited = await myca.isCenterInited('default')
     const dirExists = await isDirExists(centerPath)
 
-    assert(dirExists ? isDefaultCenterInited : ! isDefaultCenterInited)
+    assert(dirExists ? isDefaultCenterInited : !isDefaultCenterInited)
   })
 
 
