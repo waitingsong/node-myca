edaabce67a513a23edb9594c12fb9d4e87b7ebac
waiting <1661926154@qq.com>
2018-01-22 20:36:10 +0800

    test: Should initCaCert() works with blank C

--- test/40_cert.test.ts
+++ test/40_cert.test.ts
@@ -103,4 +103,25 @@ describe(filename, () => {
     }
   })
 
+  it('Should initCaCert() works with blank C', async () => {
+    const opts: myca.CaOpts = {
+      ...initialCaOpts,
+      days: 10950,
+      pass: 'mycapass',
+      CN: 'My Root CA',
+      OU: 'waitingsong.com',
+      C: 'CN',
+    }
+
+    opts.C = ''
+    try {
+      await myca.initCaCert(opts)
+      assert(false, 'initCaCert() should throw err, but NOT')
+    }
+    catch (ex) {
+      assert(true)
+    }
+  })
+
+
 })
