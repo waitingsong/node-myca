182f3b950b7d23ff26ffc4f99fc6c90503614fe2
waiting <1661926154@qq.com>
2018-01-22 20:35:18 +0800

    test: Should initCaCert() works with blank pass

--- test/40_cert.test.ts
+++ test/40_cert.test.ts
@@ -83,5 +83,24 @@ describe(filename, () => {
     }
   })
 
+  it('Should initCaCert() works with blank pass', async () => {
+    const opts: myca.CaOpts = {
+      ...initialCaOpts,
+      days: 10950,
+      pass: 'mycapass',
+      CN: 'My Root CA',
+      OU: 'waitingsong.com',
+      C: 'CN',
+    }
+
+    opts.pass = ''
+    try {
+      await myca.initCaCert(opts)
+      assert(false, 'initCaCert() should throw err, but NOT')
+    }
+    catch (ex) {
+      assert(true)
+    }
+  })
 
 })
