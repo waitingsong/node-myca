4c185cadd9fa27ec2af14e1950ef3d680f37937c
waiting <1661926154@qq.com>
2018-01-22 20:58:24 +0800

    test: Should initCaCert() works with invalid alg

--- test/40_cert.test.ts
+++ test/40_cert.test.ts
@@ -211,5 +211,25 @@ describe(filename, () => {
     }
   })
 
+  it('Should initCaCert() works with invalid alg', async () => {
+    const opts: myca.CaOpts = {
+      ...initialCaOpts,
+      days: 10950,
+      pass: 'mycapass',
+      CN: 'My Root CA',
+      OU: 'waitingsong.com',
+      C: 'CN',
+    }
+
+    opts.alg = <'rsa'> 'fake'
+    try {
+      await myca.initCaCert(opts)
+      assert(false, 'initCaCert() should throw err, but NOT')
+    }
+    catch (ex) {
+      assert(true)
+    }
+  })
+
 
 })
