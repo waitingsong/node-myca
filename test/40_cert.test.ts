8707020e667815e82876c8bd28270317d84916ca
waiting <1661926154@qq.com>
2018-01-22 20:35:02 +0800

    test: Should initCaCert() works with invalid centerName

--- test/40_cert.test.ts
+++ test/40_cert.test.ts
@@ -63,5 +63,25 @@ describe(filename, () => {
     }
   })
 
+  it('Should initCaCert() works with invalid centerName', async () => {
+    const opts: myca.CaOpts = {
+      ...initialCaOpts,
+      days: 10950,
+      pass: 'mycapass',
+      CN: 'My Root CA',
+      OU: 'waitingsong.com',
+      C: 'CN',
+    }
+
+    opts.centerName = 'fake'
+    try {
+      await myca.initCaCert(opts)
+      assert(false, 'initCaCert() should throw err, but NOT')
+    }
+    catch (ex) {
+      assert(true)
+    }
+  })
+
 
 })
