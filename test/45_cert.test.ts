4d9937316d8aa2bf90a7bc62427fdd5c5a09e27e
waiting <1661926154@qq.com>
2018-01-23 11:29:40 +0800

    test: Should genCert() works

---
+++ test/45_cert.test.ts
@@ -0,0 +1,71 @@
+/// <reference types="node" />
+/// <reference types="mocha" />
+
+import { tmpdir } from 'os'
+import { basename, join, normalize } from 'path'
+import * as assert from 'power-assert'
+import rewire = require('rewire')
+import * as rmdir from 'rimraf'
+
+import * as myca from '../src/index'
+import { isDirExists, isFileExists } from '../src/lib/common'
+import { config, initialCaOpts, initialCertOpts } from '../src/lib/config'
+
+
+const filename = basename(__filename)
+const tmpDir = tmpdir()
+const random = Math.random()
+const pathPrefix = 'myca-test-center'
+const randomPath = `${tmpDir}/${pathPrefix}-${random}`
+const mods = rewire('../src/lib/cert')
+
+console.log('----------', config.isWin32)
+config.isWin32 = process.platform === 'win32' ? true : false
+config.userHome = config.isWin32 ? normalize(process.env.USERPROFILE || '') : normalize(`${process.env.HOME}`)
+config.openssl = normalize(config.openssl)
+
+
+describe(filename, () => {
+  beforeEach(async () => {
+    const random = Math.random()
+    const randomPath = `${tmpDir}/${pathPrefix}-${random}`
+    const opts: myca.CaOpts = {
+      ...initialCaOpts,
+      days: 10950,
+      pass: 'mycapass',
+      keyBits: 2048,  // for speed
+      hash: 'sha256',
+      CN: 'My Root CA',
+      C: 'CN',
+    }
+
+    config.defaultCenterPath = `${randomPath}/${config.centerDirName}`
+    await myca.initDefaultCenter()
+    await myca.initCaCert(opts)
+  })
+  afterEach(() => {
+    rmdir(join(config.defaultCenterPath, '../'), (err) => err && console.error(err))
+  })
+
+
+  it('Should genCert() works', async () => {
+    const opts: myca.CertOpts = {
+      centerName: 'default',
+      caKeyPass: 'mycapass',
+      kind: 'server',   // server cert
+      days: 730,
+      pass: 'fooo',   // at least 4 letters
+      CN: 'www.waitingsong.com',    // Common Name
+      C: 'CN',   // Country Name (2 letter code)
+    }
+
+    try {
+      const ret: myca.IssueCertRet = await myca.genCert(opts)
+    }
+    catch (ex) {
+      return assert(false, ex)
+    }
+  })
+
+
+})
