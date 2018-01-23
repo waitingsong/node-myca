a92c5f0d1099aab77998bd27dbb59d0ed9938be0
waiting <1661926154@qq.com>
2018-01-23 11:38:44 +0800

    test: add 40_cacert-ec.test.ts

---
+++ test/40_cacert-ec.test.ts
@@ -0,0 +1,256 @@
+/// <reference types="node" />
+/// <reference types="mocha" />
+
+import { tmpdir } from 'os'
+import { basename, join, normalize } from 'path'
+import * as assert from 'power-assert'
+import rewire = require('rewire')
+import * as rmdir from 'rimraf'
+
+import * as myca from '../src/index'
+import { isDirExists, isFileExists } from '../src/lib/common'
+import { config, initialCaOpts } from '../src/lib/config'
+
+
+const filename = basename(__filename)
+const tmpDir = tmpdir()
+const pathPrefix = 'myca-test-center'
+const mods = rewire('../src/lib/cert')
+
+describe(filename, () => {
+  beforeEach(async () => {
+    const random = Math.random()
+    const randomPath = `${tmpDir}/${pathPrefix}-${random}`
+
+    config.defaultCenterPath = `${randomPath}/${config.centerDirName}`
+    await myca.initDefaultCenter()
+  })
+  afterEach(() => {
+    rmdir(join(config.defaultCenterPath, '../'), (err) => err && console.error(err))
+  })
+
+
+  it('Should initCaCert() works', async () => {
+    const opts: myca.CaOpts = {
+      ...initialCaOpts,
+      alg: 'ec',
+      days: 10950,
+      pass: 'mycapass',
+      hash: 'sha256',
+      CN: 'My Root CA',
+      OU: 'waitingsong.com',
+      C: 'CN',
+    }
+
+    try {
+      await myca.initCaCert(opts)
+    }
+    catch (ex) {
+      return assert(false, ex)
+    }
+  })
+
+  it('Should initCaCert() works with invalid param', async () => {
+    const opts: myca.CaOpts = {
+      ...initialCaOpts,
+    }
+
+    try {
+      await myca.initCaCert(opts)
+      assert(false, 'initCaCert() should throw err, but NOT')
+    }
+    catch (ex) {
+      assert(true)
+    }
+  })
+
+  it('Should initCaCert() works with blank centerName', async () => {
+    const opts: myca.CaOpts = {
+      ...initialCaOpts,
+      alg: 'ec',
+      days: 10950,
+      pass: 'mycapass',
+      CN: 'My Root CA',
+      OU: 'waitingsong.com',
+      C: 'CN',
+    }
+
+    opts.centerName = ''
+    try {
+      await myca.initCaCert(opts)
+      assert(false, 'initCaCert() should throw err, but NOT')
+    }
+    catch (ex) {
+      assert(true)
+    }
+  })
+
+  it('Should initCaCert() works with fake centerName', async () => {
+    const opts: myca.CaOpts = {
+      ...initialCaOpts,
+      alg: 'ec',
+      days: 10950,
+      pass: 'mycapass',
+      CN: 'My Root CA',
+      OU: 'waitingsong.com',
+      C: 'CN',
+    }
+
+    opts.centerName = 'fake'
+    try {
+      await myca.initCaCert(opts)
+      assert(false, 'initCaCert() should throw err, but NOT')
+    }
+    catch (ex) {
+      assert(true)
+    }
+  })
+
+  it('Should initCaCert() works with blank pass', async () => {
+    const opts: myca.CaOpts = {
+      ...initialCaOpts,
+      alg: 'ec',
+      days: 10950,
+      pass: 'mycapass',
+      CN: 'My Root CA',
+      OU: 'waitingsong.com',
+      C: 'CN',
+    }
+
+    opts.pass = ''
+    try {
+      await myca.initCaCert(opts)
+      assert(false, 'initCaCert() should throw err, but NOT')
+    }
+    catch (ex) {
+      assert(true)
+    }
+  })
+
+  it('Should initCaCert() works with blank C', async () => {
+    const opts: myca.CaOpts = {
+      ...initialCaOpts,
+      alg: 'ec',
+      days: 10950,
+      pass: 'mycapass',
+      CN: 'My Root CA',
+      OU: 'waitingsong.com',
+      C: 'CN',
+    }
+
+    opts.C = ''
+    try {
+      await myca.initCaCert(opts)
+      assert(false, 'initCaCert() should throw err, but NOT')
+    }
+    catch (ex) {
+      assert(true)
+    }
+  })
+
+  it('Should initCaCert() works with blank CN', async () => {
+    const opts: myca.CaOpts = {
+      ...initialCaOpts,
+      alg: 'ec',
+      days: 10950,
+      pass: 'mycapass',
+      CN: 'My Root CA',
+      OU: 'waitingsong.com',
+      C: 'CN',
+    }
+
+    opts.CN = ''
+    try {
+      await myca.initCaCert(opts)
+      assert(false, 'initCaCert() should throw err, but NOT')
+    }
+    catch (ex) {
+      assert(true)
+    }
+  })
+
+  it('Should initCaCert() works with zero days', async () => {
+    const opts: myca.CaOpts = {
+      ...initialCaOpts,
+      alg: 'ec',
+      days: 10950,
+      pass: 'mycapass',
+      CN: 'My Root CA',
+      OU: 'waitingsong.com',
+      C: 'CN',
+    }
+
+    opts.days = 0
+    try {
+      await myca.initCaCert(opts)
+      assert(false, 'initCaCert() should throw err, but NOT')
+    }
+    catch (ex) {
+      assert(true)
+    }
+  })
+
+  it('Should initCaCert() works with negative days', async () => {
+    const opts: myca.CaOpts = {
+      ...initialCaOpts,
+      alg: 'ec',
+      days: 10950,
+      pass: 'mycapass',
+      CN: 'My Root CA',
+      OU: 'waitingsong.com',
+      C: 'CN',
+    }
+
+    opts.days = -1
+    try {
+      await myca.initCaCert(opts)
+      assert(false, 'initCaCert() should throw err, but NOT')
+    }
+    catch (ex) {
+      assert(true)
+    }
+  })
+
+  it('Should initCaCert() works with invalid alg', async () => {
+    const opts: myca.CaOpts = {
+      ...initialCaOpts,
+      alg: 'ec',
+      days: 10950,
+      pass: 'mycapass',
+      CN: 'My Root CA',
+      OU: 'waitingsong.com',
+      C: 'CN',
+    }
+
+    opts.alg = <'rsa'> ''
+    try {
+      await myca.initCaCert(opts)
+      assert(false, 'initCaCert() should throw err, but NOT')
+    }
+    catch (ex) {
+      assert(true)
+    }
+  })
+
+  it('Should initCaCert() works with invalid hash', async () => {
+    const opts: myca.CaOpts = {
+      ...initialCaOpts,
+      alg: 'ec',
+      days: 10950,
+      pass: 'mycapass',
+      CN: 'My Root CA',
+      OU: 'waitingsong.com',
+      C: 'CN',
+    }
+
+    opts.hash = <'sha256'> 'fake'
+    try {
+      await myca.initCaCert(opts)
+      assert(false, 'initCaCert() should throw err, but NOT')
+    }
+    catch (ex) {
+      assert(true)
+    }
+  })
+
+})
