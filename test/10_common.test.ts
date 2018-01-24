add2867c3931d747dcd501e1fe865c735c130561
waiting <1661926154@qq.com>
2018-01-24 19:01:15 +0800

    test: Should createDir() works

---
+++ test/10_common.test.ts
@@ -0,0 +1,38 @@
+/// <reference types="node" />
+/// <reference types="mocha" />
+
+import { tmpdir } from 'os'
+import { basename, join } from 'path'
+import * as assert from 'power-assert'
+import * as rmdir from 'rimraf'
+
+import * as myca from '../src/index'
+import { createDir, isDirExists } from '../src/lib/common'
+import { config } from '../src/lib/config'
+
+const filename = basename(__filename)
+const tmpDir = tmpdir()
+const pathPrefix = 'myca-test-center'
+
+
+describe(filename, () => {
+  it('Should createDir() works', async () => {
+    const random = Math.random()
+    const randomPath = `${tmpDir}/${pathPrefix}-${random}`
+
+    try {
+      await createDir(randomPath)
+    }
+    catch (ex) {
+      return assert(false, ex)
+    }
+
+    if ( ! await isDirExists(randomPath)) {
+      return assert(false, `folder not exists, path: "${randomPath}"`)
+    }
+
+    rmdir(randomPath, (err) => err && console.error(err))
+  })
+
+
+})
