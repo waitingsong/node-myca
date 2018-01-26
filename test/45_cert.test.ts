99a9c1a13874c8e55f3478bc1642c1872b86b2fb
waiting <1661926154@qq.com>
2018-01-26 21:04:37 +0800

    test: Should processIssueOpts() works with invalid lesser keyBits

--- test/45_cert.test.ts
+++ test/45_cert.test.ts
@@ -412,6 +412,36 @@ describe(filename, () => {
   })
 
 
+  it('Should processIssueOpts() works with invalid lesser keyBits', async () => {
+    const opts: myca.CertOpts = {
+      centerName: 'default',
+      caKeyPass: 'mycapass',
+      kind: 'server',   // server cert
+      days: 730,
+      pass: 'fooo',   // at least 4 letters
+      CN: 'www.waitingsong.com',    // Common Name
+      C: 'CN',   // Country Name (2 letter code)
+      keyBits: 2047,
+      alg: 'rsa',   // must specify
+    }
+    const fnName = 'processIssueOpts'
+    const fn = <(config: myca.Config, options: myca.IssueOpts) => Promise<myca.IssueOpts>> mods.__get__(fnName)
+
+    if (typeof fn !== 'function') {
+      return assert(false, `${fnName} is not a function`)
+    }
+
+    try {
+      const ret = await fn(config, opts)
+
+      assert(ret.keyBits === 2048, `processed keyBits value should be 2048, but got "${ret.keyBits}"`)
+    }
+    catch (ex) {
+      return assert(false, ex)
+    }
+  })
+
+
 
   // --------------
 
