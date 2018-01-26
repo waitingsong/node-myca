4c56556355ad103203eb0435cee5d7f9c662a8b9
waiting <1661926154@qq.com>
2018-01-26 20:44:43 +0800

    fix: comparison within nextSerial()

--- src/lib/center.ts
+++ src/lib/center.ts
@@ -27,7 +27,7 @@ export async function nextSerial(centerName: string, config: Config): Promise<st
   if (typeof nextDec !== 'number' ||
     ! nextDec ||
     ! Number.isSafeInteger(nextDec) ||
-    nextHex !== nextDec.toString(16) ||
+    nextHex.replace(/^0+/, '') !== nextDec.toString(16) ||
     nextDec < 1
   ) {
     throw new Error(`retrive nextSerial failed or invalid. value: "${nextHex}", Dec: ${nextDec}`)
