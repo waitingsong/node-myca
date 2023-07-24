b2475cbba77451851804855742c3c4ff7e4ca199
waiting <1661926154@qq.com>
2023-07-24 18:15:19 +0800

    test(myca-cli): update test/31.cli-src.test.ts

--- packages/myca-cli/test/31.cli-src.test.ts
+++ packages/myca-cli/test/31.cli-src.test.ts
@@ -1,6 +1,7 @@
 import { rm, rename } from 'fs/promises'
 import assert from 'node:assert/strict'
 import { join } from 'node:path'
+import { sep } from 'path'
 
 import {
   userHome,
@@ -14,7 +15,7 @@ import { runCmd, RunCmdArgs } from '../src/index.js'
 
 
 const defaultCenterPath = join(userHome, '.myca')
-let defaultCenterPathBak = join(userHome, '.myca-' + Math.random().toString())
+let defaultCenterPathBak = join(userHome, '.myca-bak-' + Math.random().toString())
 const initArgs: RunCmdArgs = {
   cmd: 'init',
   options: null,
@@ -98,4 +99,58 @@ describe(fileShortPath(import.meta.url), () => {
     })
   })
 
+  describe('Should issue work', () => {
+    if (! isVersionMatch) {
+      console.info(`Skip test, required node version: ${requiredVersion}, current version: ${currentVersion}`)
+      return
+    }
+
+    it('server', async () => {
+      const cmd = 'issue'
+      const opts = {
+        kind: 'server',
+        ips: '127.0.0.1, 192.168.0.1',
+        SAN: 'localhost',
+        centerName: 'default',
+        alg: 'ec',
+        caKeyPass: 'mycapass',
+
+        days: 10950,
+        pass: 'mycapass',
+        CN: 'test',
+        O: 'it',
+        C: 'CN',
+      }
+      const args: (string|number)[] = [
+        '--kind', opts.kind,
+        '--ips', opts.ips,
+        '--SAN', opts.SAN,
+        '--centerName', opts.centerName,
+        '--alg', opts.alg,
+        '--caKeyPass', opts.caKeyPass,
+
+        '--days', opts.days,
+        '--pass', opts.pass,
+        '--CN', opts.CN,
+        '--O', opts.O,
+        '--C', opts.C,
+      ]
+
+      await $`pwd`
+      const { stdout, stderr, exitCode } = await $`node --enable-source-maps --loader ts-node/esm ${cli} ${cmd} ${args} `
+      if (exitCode !== 0) {
+        console.error({ stderr })
+      }
+      // const { stdout } = await $`ts-node-esm ${cli} ${cmd} ${args} `
+      assert(stdout)
+      assert(stdout.includes('Issue a Certificate with:'), stdout)
+      assert(stdout.includes('pubKey:'), stdout)
+      assert(stdout.includes('-----BEGIN PUBLIC KEY-----'), stdout)
+      assert(stdout.includes('pass: "mycapass"'), stdout)
+      assert(stdout.includes('privateKeyFile'), stdout)
+      assert(stdout.includes(opts.kind + sep + '01.key'), stdout)
+      assert(stdout.includes('privateUnsecureKeyFile'), stdout)
+      assert(stdout.includes(opts.kind + sep + '01.key.unsecure'), stdout)
+    })
+  })
 })
